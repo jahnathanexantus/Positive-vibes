d3b25dca2d12113753981c31e226930f0d173275
jahnathan x <jahnathanexantus@gmail.com>
2024-10-03 20:11:05 -0400

    added contact"

--- positive-vibes/src/pages/home/Home.jsx
+++ positive-vibes/src/pages/home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Profile from '../../assets/images/S-24.jpg'
 import { Link } from 'react-router-dom'
-import { FaArrowRight } from 'react-icons/fa'
+import { FaArrowRight, FaPhoneAlt, FaEnvelope } from 'react-icons/fa'
 import './home.css'
 
 const Home = () => {
@@ -12,8 +12,8 @@ const Home = () => {
       <div className='home__content'>
         <div className='home__data'>
           <h1 className='home__title'>
-            <span>Welcome to Positive Vibes-FOTP</span> <br></br>Focused on the
-            Positive, Empowering Your Journey
+            <span>Welcome to Positive Vibes-FOTP</span> <br />
+            Focused on the Positive, Empowering Your Journey
           </h1>
           <p className='home__description'>
             With a passion for empowering individuals and a commitment to
@@ -21,18 +21,29 @@ const Home = () => {
             environment where you can thrive. My diverse skill set ensures that
             whether you’re seeking guidance, inspiration, or a memorable event,
             you’ll receive exceptional service tailored to your needs.
-            <br></br>
+            <br />
             Discover the possibilities with Positive Vibes-FOTP. Your
             transformation starts here!
           </p>
           <Link to='/about' className='button'>
-            More About Me{''}
+            More About Me
             <span className='button__icon'>
               <FaArrowRight />
             </span>
           </Link>
         </div>
+
+        {/* Icons for Phone and Email */}
+        <div className='contact-icons'>
+          <a href='tel:+4079137687' className='contact-icon'>
+            <FaPhoneAlt />
+          </a>
+          <a href='mailto:positivevibesfotp@gmail.com' className='contact-icon'>
+            <FaEnvelope />
+          </a>
+        </div>
       </div>
+
       <div className='color__block'></div>
     </section>
   )
